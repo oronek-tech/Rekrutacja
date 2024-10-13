6b590174ca73f33ef5be66eefbbb87f0c6cac8a5
Piotr Oronowicz <piotroronowicz@gmail.com>
2024-10-13 19:57:33 +0200

    Zadanie 2.

--- Rekrutacja/Rekrutacja/Helpers/ActionHelper.cs
+++ Rekrutacja/Rekrutacja/Helpers/ActionHelper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Rekrutacja.Models;
+using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,31 @@ namespace Rekrutacja.Helpers
             else
                 throw new Exception("Niepoprawny operator");
             return wynik;
+        }
+        public static int ObliczPole(double valueA, double valueB, FigureType typFigury)
+        {
+            
+            switch (typFigury)
+            {
+                case FigureType.Kwadrat:
+                    if (valueA <= 0)
+                        throw new Exception("Wartość boku kwadratu musi być dodatnia!");
+                    return (int)Math.Round(valueA * valueA);
+                case FigureType.Prostokąt:
+                    if (valueA <= 0 || valueB <= 0)
+                        throw new Exception("Wartości boków prostokąta muszą być dodatnie!");
+                    return (int)Math.Round(valueA * valueB);
+                case FigureType.Trojkat:
+                    if (valueA <= 0 || valueB <= 0)
+                        throw new Exception("Wartości podstawy i wysokości trójkąta muszą być dodatnie!");
+                    return (int)Math.Round(valueA * valueB/2);
+                case FigureType.Kolo:
+                    if (valueA <= 0)
+                        throw new Exception("Wartość promienia koła musi być dodatnia!");
+                    return (int)Math.Round(Math.PI*valueA * valueA);
+                default:
+                    throw new Exception("Nie wybrano typu figury");
+            }
         }
     }
 }
---
+++ Rekrutacja/Rekrutacja/Workers/Template/FieldTemplateWorker.cs
@@ -0,0 +1,70 @@
+﻿using Soneta.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Soneta.Kadry;
+using Soneta.KadryPlace;
+using Soneta.Types;
+using Rekrutacja.Workers.Template;
+using Rekrutacja.Helpers;
+using Rekrutacja.Models;
+
+[assembly: Worker(typeof(FieldTemplateWorker), typeof(Pracownicy))]
+namespace Rekrutacja.Workers.Template
+{
+    public class FieldTemplateWorker
+    {
+        public class FieldTemplateWorkerParametry : ContextBase
+        {
+            [Caption("A")]
+            public double A { get; set; }
+            [Caption("B")]
+            public double B { get; set; }
+            [Caption("Data obliczeń")]
+            public Date DataObliczen { get; set; }
+            [Caption("Typ figury")]
+            public FigureType TypFigury { get; set; }
+            public FieldTemplateWorkerParametry(Context context) : base(context)
+            {
+                this.DataObliczen = Date.Today;
+            }
+        }
+        [Context]
+        public Context Cx { get; set; }
+        [Context]
+        public FieldTemplateWorkerParametry Parametry { get; set; }
+        [Action("Kalkulator pola",
+           Description = "Prosty kalkulator do wyliczania pola figury",
+           Priority = 10,
+           Mode = ActionMode.ReadOnlySession,
+           Icon = ActionIcon.Accept,
+           Target = ActionTarget.ToolbarWithText)]
+        public void WykonajAkcje()
+        {
+            DebuggerSession.MarkLineAsBreakPoint();
+            Pracownik[] pracowniks = null;
+            if (this.Cx.Contains(typeof(Pracownik[])))
+                pracowniks = (Pracownik[])this.Cx[typeof(Pracownik[])];
+
+            using (Session nowaSesja = this.Cx.Login.CreateSession(false, false, "ModyfikacjaPracownika"))
+            {
+                using (ITransaction trans = nowaSesja.Logout(true))
+                {
+                    var dataObliczen = this.Parametry.DataObliczen;
+                    var wynik = ActionHelper.ObliczPole(this.Parametry.A, this.Parametry.B, this.Parametry.TypFigury);
+                    foreach (var p in pracowniks)
+                    {
+                        var pracownikZSesja = nowaSesja.Get(p);
+                        pracownikZSesja.Features["Wynik"] = (double)wynik;
+                        pracownikZSesja.Features["DataObliczen"] = dataObliczen;
+                    }
+                    trans.CommitUI();
+                }
+                nowaSesja.Save();
+            }
+        }
+
+    }
+}
